b4a68a3bc582a44d07c3eb77b6d15c1f0a6fc4dd
1827009 <43544451+1827009@users.noreply.github.com>
2018-12-01 01:32:17 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 int main(int argc, char *argv[]) 
 {
     char str[7];
+    int i;
+    for(i=0;i<7;i++)
+    {
+        str[i]=*argv[i];
+    }
     
     while(fgets(str, sizeof(str), stdin)){
         printf("%s", str);
